c8ab6b5381043f0d431f02755b64aa59ddad0053
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-05-06 20:48:27 +0200

    Dosanie klas:
    -Gra
    -StanGry

---
+++ Kasyno/Kasyno/Gra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasyno
+{
+    class Gra
+    {
+        public int id { get; set; }
+        public string nazwa { get; set; }
+    }
+}
---
+++ Kasyno/Kasyno/StanGry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasyno
+{
+    class StanGry
+    {
+        public Gra gra { get; set; }
+        public int ilosc { get; set; }
+        public DateTimeOffset dataUruchomienia { get; set; }
+    }
+}
