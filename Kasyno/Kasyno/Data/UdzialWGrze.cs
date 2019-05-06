310e0c2c37bb4897e5272ddb14054f70c403d19a
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-05-06 20:53:55 +0200

    - dodanie klasy Udzial w grze będącej eventem - przeniesienie wszystkich dotyczczasowych klas do katalogu Data

---
+++ Kasyno/Kasyno/Data/UdzialWGrze.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasyno.Data
+{
+    class UdzialWGrze
+    {
+        public StanGry stangry {get; set;}
+        public Klient klient {get; set }
+    }
+}
