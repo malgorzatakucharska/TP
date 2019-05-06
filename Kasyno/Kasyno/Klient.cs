dab05d7f11ccbc1e11679074ce7cba11b0e6ec4e
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-05-06 20:42:19 +0200

    uzupelnienie klasy klient

---
+++ Kasyno/Kasyno/Klient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasyno
+{
+    public class Klient
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int wiek { get; set; }
+        public string adresEmail { get; set; }
+    }
+}
