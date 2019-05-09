781863e9bc7f424a9beb02a5ad66e5a72126ae14
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-05-09 17:41:03 +0200

    Dodanie: metod gey dla mojedynczych elementów metod getall zwarajacych liste

--- Kasyno/Kasyno/MainLogic/DataRepository.cs
+++ Kasyno/Kasyno/MainLogic/DataRepository.cs
@@ -34,5 +34,67 @@ namespace Kasyno.MainLogic
         {
             data.udzialywgrze.Add(udzial);
         }
+        //metody Get
+        public Klient GetKlient(int i)
+        {
+            if (data.klienci.Count > i)
+            {
+                return data.klienci[i];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public Gra GetGra(int i)
+        {
+            if (data.gry.ContainsKey(i))
+            {
+                return data.gry[i];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public StanGry GetStanGry(int i)
+        {
+            if (data.stanygier.Count > i)
+            {
+                return data.stanygier[i];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public UdzialWGrze GetUdzialWGrze(int i)
+        {
+            if (data.udzialywgrze.Count > i)
+            {
+                return data.udzialywgrze[i];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        //metody GetAll
+        public IEnumerable<Klient> GetAllKlienci()
+        {
+            return data.klienci;
+        }
+        public IEnumerable<Gra> GetAllGry()
+        {
+            return data.gry.Values;
+        }
+        public IEnumerable<StanGry> GetAllStanyGier()
+        {
+            return data.stanygier;
+        }
+        public IEnumerable<UdzialWGrze> GetAllUdzialyWGrze()
+        {
+            return data.udzialywgrze;
+        }
     }
     }
