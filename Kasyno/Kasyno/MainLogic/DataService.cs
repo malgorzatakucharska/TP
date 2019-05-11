2b8a1c4ce5184011d656d331b30ea7bf50c72895
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-05-11 13:47:52 +0200

    Dodanie klasy DadaService

---
+++ Kasyno/Kasyno/MainLogic/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasyno.MainLogic
+{
+    class DataService
+    {
+        private DataRepository repository;
+
+        public DataService(DataRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
