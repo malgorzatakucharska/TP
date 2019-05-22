c1d421e8a6da1882b7de2d916d3589d955478e91
maszkowskaewa <maszkowska.ewa@vp.pl>
2019-05-22 23:37:50 +0200

    Dołączono folder MVVM i Model do projektu GUI utworzenie klasy ViewModelBAse utworzenie klasy CustomerModel

---
+++ Kasyno/GUI/MinimalMVVM/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+
+namespace GUI.MinimalMVVM
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyEventChanged(string propertyName = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Kasyno/GUI/Model/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLayer;
+using DataRepository;
+
+namespace GUI.Model
+{
+    public class CustomerModel
+    {
+        public IEnumerable<Customer> Customer
+        {
+            get { return DataRepository.DataRepository.SelectAllCustomer(); }
+        }
+    }
+}
