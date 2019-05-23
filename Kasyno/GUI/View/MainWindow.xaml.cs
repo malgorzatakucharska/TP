f55e66de36889faccaf21b9be1d6e19e94df04d8
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-05-23 12:32:40 +0200

    dodanie w warstwie view MainWindow

--- Kasyno/GUI/View/MainWindow.xaml.cs
+++ Kasyno/GUI/View/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI
+namespace GUI.View
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
